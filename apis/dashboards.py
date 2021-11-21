a70adc67406db3166c95e0645ba200d3755157ef
Aouatef Mareghni <aouatef_mareghni@hotmail.com>
2021-11-21 12:40:04 +0100

    added admin stats

--- apis/dashboards.py
+++ apis/dashboards.py
@@ -1,5 +1,7 @@
 from flask import jsonify
 
+from models.user import User
+from permissions import authorization_required
 from services.dashboard import get_general_stats
 from settings import app
 
@@ -8,3 +10,8 @@ from settings import app
 def get_general_stats_api():
     return jsonify(get_general_stats())
 
+
+@app.route('/dashboards/vendor_stats', methods=['GET'])
+@authorization_required()
+def get_vendor_general_stats_api(current_user: User):
+    return jsonify(get_general_stats(store_id=current_user.store_id))
--- services/dashboard.py
+++ services/dashboard.py
@@ -6,24 +6,37 @@ from pprint import pprint
 from google.cloud import firestore
 
 from firebase import fs, COLLECTIONS as COL
-from services.product import get_all_products
+from services.product import get_all_products, get_products_by_store
 from services.store import get_stores_list
 from services.user import get_all_users
 
 
-def get_general_stats() -> dict:
+def get_general_stats(store_id: str = None) -> dict:
     result = dict()
-    result['total_products'] = len(get_all_products())
-    result['total_stores'] = len(get_stores_list())
-    result['total_users'] = len(get_all_users())
+    top_stores = dict()
+
+    if store_id is None:
+        result['total_products'] = len(get_all_products())
+        result['total_stores'] = len(get_stores_list())
+        result['total_users'] = len(get_all_users())
+        result['top_stores'] = dict()
+
+
+        orders = fs.collection(COL['orders']) \
+            .order_by("order_date", direction=firestore.Query.DESCENDING).stream()
+    else:
+        result['total_products'] = len(get_products_by_store(store_id))
+
+        orders = fs.collection(COL['orders']) \
+            .where("store_id", "==", store_id) \
+            .order_by("order_date", direction=firestore.Query.DESCENDING).stream()
+
     result['total_orders'] = 0
     result['total_sales'] = 0
-    result['top_stores'] = dict()
     result['top_products'] = dict()
     daily_sales_products = {}
     monthly_sales_products = {}
-    top_stores = dict()
-    orders = fs.collection(COL['orders']).order_by("order_date", direction=firestore.Query.DESCENDING).stream()
+
     orders_docs = []
 
     for o in orders:
@@ -45,9 +58,10 @@ def get_general_stats() -> dict:
         monthly_sales_products[month_year]['products'] += len(o.get('products'))
         monthly_sales_products[month_year]['sales'] += o.get('total_price')
 
-        if o.get('store_id') not in top_stores:
-            top_stores[o['store_id']] = 0
-        top_stores[o['store_id']] += o.get('total_price')
+        if store_id is None:
+            if o.get('store_id') not in top_stores:
+                top_stores[o['store_id']] = 0
+            top_stores[o['store_id']] += o.get('total_price')
 
         # top_stores[o['store_id']]['orders'] += 1
     pprint(top_stores)
@@ -68,7 +82,7 @@ def get_general_stats() -> dict:
     result['sale_per_day'] = result.get('total_sales') / total_days
     result['days_since_first_order'] = total_days
     # result['top_stores'] = {k: v for k, v in sorted(top_stores.items(), key=lambda item: item[1])}
-    result['top_stores'] = top_stores
-
+    if store_id is None:
+        result['top_stores'] = top_stores
 
     return result
